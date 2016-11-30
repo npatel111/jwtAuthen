9a374b2128643a2437a101c0e5c7ed1e94d4c521
Niti Nrw <niti.patel@flatironschool.com>
2016-11-30 14:54:39 -0500

    d

---
+++ lib/auth.rb
@@ -0,0 +1,15 @@
+require 'jwt'
+
+class Auth
+  ALGORITHM = 'HS256'
+  def self.issue(payload)
+    JWT.encode (payload, Rails.application.secrets[:secret_key_base], ALGORITHM)
+  end
+
+  def self.decode(token)
+    JWT.decode (token, Rails.application.secrets[:secret_key_base], true, {algorithm: ALGORITHM}).first
+  end
+
+
+
+end
